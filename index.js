c6c88380ef2684e8766c4e9622762f144f29a4f6
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-23 14:14:25 -0500

    Adds blog functionality

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const http = require('http');
 const fs = require('fs');
+const blog = require('./blog.js');
+
+const header = fs.readFileSync('templates/header.html');
+const footer = fs.readFileSync('templates/footer.html');
+const nav = fs.readFileSync(`templates/nav.html`);
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html' 
-    });
+    
+    if(req.url === "/blog") {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        const blogContent = `${blog.getContent()}`;
 
-    const content = '<h1>Hello!</h1>';
-    res.end(content);
+        res.end(header + nav + blogContent + footer);
+    } else if (req.url === "/") {
+        res.writeHead(200, {
+            'Content-Type': 'text/html' 
+        });
+    
+        const content = `<h1>Hello!</h1><h2>It is ${new Date()}</h2>`;
+        res.end(header + nav + content + footer);
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html'
+        })
+
+        res.end('404: File not found')
+    }
 });
 
 server.listen(3000, () => {
