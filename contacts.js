640f81dfad9f49453a2652ea038d6fb6c9c1c402
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-23 15:40:08 -0500

    adds random contact functionality

--- contacts.js
+++ contacts.js
@@ -1,4 +1,5 @@
 const friends = require('./contacts.json');
+const faker = require('faker');
 
 function getContacts() {
     let result = '';
@@ -8,8 +9,22 @@ function getContacts() {
     return result;
 }
 
+function getContacts2(times = 5) {
+    let result = '';
+    let count = 0;
+    while (count < times) {
+        let contactCard = faker.helpers.createCard();
+        result += `<h1>${contactCard.name}</h1>\n<h2>${contactCard.phone}, ${contactCard.email}</h2>\n`;
+        count += 1;
+    }
+    return result;
+}
+
 // console.log(getContacts());
+// console.log(getContacts2());
+// console.log(faker.helpers.createCard());
 
 module.exports = {
-    getContacts
+    getContacts,
+    getContacts2
 };
