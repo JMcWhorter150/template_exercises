ec92abd7b8ec8c767f51c288040844126ada21b8
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-23 15:01:03 -0500

    Adds lorem ipsum functionality

--- blog.js
+++ blog.js
@@ -1,8 +1,21 @@
-function getContent() {
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
+});
+
+function getContent(times = 10) {
     let result = '';
     let count = 0;
-    while (count < 10) {
-        result += '<p>What an amazing blog!</p>';
+    while (count < times) {
+        result += `<p>${lorem.generateParagraphs(1)}</p>`;
         count += 1;
     }
     return result;
--- index.js
+++ index.js
@@ -7,28 +7,15 @@ const footer = fs.readFileSync('templates/footer.html');
 const nav = fs.readFileSync(`templates/nav.html`);
 
 const server = http.createServer((req, res) => {
-    
-    if(req.url === "/blog") {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        const blogContent = `${blog.getContent()}`;
-
-        res.end(header + nav + blogContent + footer);
-    } else if (req.url === "/") {
         res.writeHead(200, {
             'Content-Type': 'text/html' 
         });
     
-        const content = `<h1>Hello!</h1><h2>It is ${new Date()}</h2>`;
+        let content = `<h1>Hello!</h1><h2>It is ${new Date()}</h2>`;
+        if (req.url === "/blog") {
+            content = `${blog.getContent(5)}`;
+        }
         res.end(header + nav + content + footer);
-    } else {
-        res.writeHead(404, {
-            'Content-Type': 'text/html'
-        })
-
-        res.end('404: File not found')
-    }
 });
 
 server.listen(3000, () => {
